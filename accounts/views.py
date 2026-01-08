3dad0d19f143c3937a6331aea38b3af34c3567ad
Darshan801 <darshankunwar79@gmail.com>
2026-01-08 10:16:47 +0545

    signin validation

--- accounts/views.py
+++ accounts/views.py
@@ -13,20 +13,20 @@ def signup(request):
         email=request.POST['email']
         password=request.POST['pass1']
         confirm_password=request.POST['pass2']
-        #password validation
-        # if(
-        #     len(password) < 8 or 
-        #     not re.search(r"[A-Za-z]",password) or
-        #     not re.search(r"[0-9]",password) or
-        #     not re.search(r"[!@#$%^&*(),.?\":{}|<>]",password)
-        # ):
-        #     messages.warning(request,"Password must be at least 8 characters long and contain a letter, a number, and a special character")
-        #     return render(request,'signup.html')
-        # # password match check
-        # if password != confirm_password:
-        #     messages.warning(request,"password don't  match")
-        #     # return HttpResponse("password dont match")
-        #     return render(request,'signup.html')
+        # password validation
+        if(
+            len(password) < 8 or 
+            not re.search(r"[A-Za-z]",password) or
+            not re.search(r"[0-9]",password) or
+            not re.search(r"[!@#$%^&*(),.?\":{}|<>]",password)
+        ):
+            messages.warning(request,"Password must be at least 8 characters long and contain a letter, a number, and a special character")
+            return render(request,'signup.html')
+        # password match check
+        if password != confirm_password:
+            messages.warning(request,"password don't  match")
+            # return HttpResponse("password dont match")
+            return render(request,'signup.html')
         try:
             if User.objects.get(username=email):
                 # return HttpResponse("email already exitsts")
